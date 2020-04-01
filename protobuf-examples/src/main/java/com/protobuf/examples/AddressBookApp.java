c84f59c37ef03ff81180acedc2a1919b5fcefe1a
ramuramaiah <ramu.ramaiah@gmail.com>
2020-04-01 17:20:01 +0530

    Updated AddressBookApp.java

--- protobuf-examples/src/main/java/com/protobuf/examples/AddressBookApp.java
+++ protobuf-examples/src/main/java/com/protobuf/examples/AddressBookApp.java
@@ -1,61 +1,13 @@
 package com.protobuf.examples;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 
 import com.protobuf.examples.AddressBookSerDe.AddressBook;
 import com.protobuf.examples.AddressBookSerDe.Person;
 
 public class AddressBookApp {
-	  // This function fills in a Person message based on user input.
-	  static Person PromptForAddress(BufferedReader stdin,
-	                                 PrintStream stdout) throws IOException {
-	    Person.Builder person = Person.newBuilder();
-
-	    stdout.print("Enter person ID: ");
-	    person.setId(Integer.valueOf(stdin.readLine()));
-
-	    stdout.print("Enter name: ");
-	    person.setName(stdin.readLine());
-
-	    stdout.print("Enter email address (blank for none): ");
-	    String email = stdin.readLine();
-	    if (email.length() > 0) {
-	      person.setEmail(email);
-	    }
-
-	    while (true) {
-	      stdout.print("Enter a phone number (or leave blank to finish): ");
-	      String number = stdin.readLine();
-	      if (number.length() == 0) {
-	        break;
-	      }
-
-	      Person.PhoneNumber.Builder phoneNumber =
-	        Person.PhoneNumber.newBuilder().setNumber(number);
-
-	      stdout.print("Is this a mobile, home, or work phone? ");
-	      String type = stdin.readLine();
-	      if (type.equals("mobile")) {
-	        phoneNumber.setType("mobile");
-	      } else if (type.equals("home")) {
-	        phoneNumber.setType("home");
-	      } else if (type.equals("work")) {
-	        phoneNumber.setType("work");
-	      } else {
-	        stdout.println("Unknown phone type.  Using default.");
-	      }
-
-	      person.addPhones(phoneNumber);
-	    }
-
-	    return person.build();
-	  }
-	  
 	  // Main function:  Reads the entire address book from a file,
 	  //   adds one person based on user input, then writes it back out to the same
 	  //   file.
